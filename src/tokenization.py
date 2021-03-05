f059628b7918146ef972d29ee66bb88b7bbf0d93
BoxinHuang <45488458+Pakyen@users.noreply.github.com>
2021-03-05 15:23:02 +0000

    Update tokenization.py

--- src/tokenization.py
+++ src/tokenization.py
@@ -3,13 +3,13 @@ tokenization：
 1. 将sentences进行分词
 2. 并去除了一些停用词    , :  " ' ! ? 's a the an
 输入：一个list，这个list由很多个句子组成 ['sentences1', 'sentences2', ...]
-输出：list，由token组成
+输出：一个token list，由token组成；一个token_of_sentences list，二维数组，包含了每个句子的token
 '''
 '''
 该文件的使用，基于preProcessing，流程：
 1. 划分：labels, sentences = sentenceProcessing(conf.get('param', 'path_train'))
 2. 小写化首字母：sentences = lower_first_letter(sentences)
-3. Tokenization: tokens = tokenization(sentences)
+3. Tokenization: tokens,token_of_sentences = tokenization(sentences)
 '''
 
 from src.pre_processing import sentence_processing, lower_first_letter
@@ -18,8 +18,11 @@ from src.pre_processing import conf
 
 def tokenization(sentences):
     # split using whitespace
+    token_of_sentences = []
     for i in range(len(sentences)):
         sentences[i] = sentences[i].split(' ')
+        token_of_sentences.append(list(filter(lambda x: x!='?',sentences[i])))
+
     # 2-d array -> 1-d array
     sentences = [b for a in sentences for b in a]
     # 进一步去除: , :  " ' ! ? 's a the an
@@ -30,7 +33,7 @@ def tokenization(sentences):
             sentences[i] = ''
     # 删除空元素''和'``'
     sentences = [i for i in sentences if i != '' and i != '``']
-    return sentences
+    return sentences,token_of_sentences
 
 
 '''
@@ -39,6 +42,7 @@ Tokenization 使用流程：
 if __name__ == '__main__':
     labels, sentences = sentence_processing(conf.get('param', 'path_train'))
     sentences = lower_first_letter(sentences)
-    tokens = tokenization(sentences)
+    tokens,token_of_sentences = tokenization(sentences)
 
     print('tokens: ', tokens)
+    print('token_of_sentences: ', token_of_sentences)
