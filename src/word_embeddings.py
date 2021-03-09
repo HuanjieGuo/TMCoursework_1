c49bf1d12c377833d9e9c63e0bd09624ef1e4bdc
Jacky Guo <huanjie.guo@outlook.com>
2021-03-09 09:26:12 +0000

    修改了initialised_word_vector的定义，添加了对模型的训练验证

--- src/word_embeddings.py
+++ src/word_embeddings.py
@@ -20,19 +20,19 @@ def randomly_initialised_vectors(tokens=None,threshold=None):
     for k in list(wordCountDict.keys()):  # 对字典a中的keys，相当于形成列表list
         if wordCountDict[k] < threshold:
             del wordCountDict[k]
-    # print(wordCountDict)
     wordToIx = {}
     wordToIx['UNK'] = 0
     i = 1
     for key in wordCountDict.keys():
         wordToIx[key] = i
         i = i+1
-    embeds = nn.Embedding(len(wordToIx), int(conf.get("param","word_embedding_dim")))  # 2 words in vocab, 100 dimensional embeddingsr
+    # embeds = nn.Embedding(len(wordToIx), int(conf.get("param","word_embedding_dim")))  # 2 words in vocab, 100 dimensional embeddingsr
     wordVectors = []
-    for key in wordToIx:
-        lookup_tensor = torch.tensor([wordToIx[key]], dtype=torch.long)
-        embed = embeds(lookup_tensor)
-        wordVectors.append(embed[:, :].tolist()[0])
+    for _ in wordToIx:
+        wordVectors.append(np.random.random(int(conf.get("param","word_embedding_dim"))))
+    # for key in wordToIx:
+    #     lookup_tensor = torch.tensor([wordToIx[key]], dtype=torch.long)
+    #     embed = embeds(lookup_tensor)
+    #     wordVectors.append(embed[:, :].tolist()[0])
+    print(np.array(wordVectors))
     return np.array(wordVectors),wordToIx
-
-
