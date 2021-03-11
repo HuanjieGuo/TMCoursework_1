ed7012c9718bd89a549ba7e371c641cd9575dba8
Maxine1997 <maxine19970205@163.com>
2021-03-11 18:09:22 +0000

    update word2vec

--- to_be_merged/preprocessing.py
+++ to_be_merged/preprocessing.py
@@ -1,5 +1,44 @@
-# import globals
+# By Maxine
+
 from collections import Counter
+import word2vec
+
+
+def process_train_set(path):
+    with open(path, 'r') as f:
+        data = f.readlines()
+        labels, sentences = labels_extraction(data)
+        # print(sentences[1])
+        # print(len(sentences))
+        sentences = remove_punctuations(sentences)
+        sentences = to_lower_case(sentences)
+        sorted_words = make_vocabulary(sentences)
+        word_idx, _ = word2vec.create_dict(sorted_words)
+        sentences_in_idx = word2vec.replace_words_with_idx(sentences, word_idx)
+        label_idx = get_label_idx(labels)
+        labels_in_idx = replace_labels_with_idx(labels, label_idx)
+        return sentences_in_idx, labels_in_idx, word_idx, label_idx
+
+
+def process_new_dataset(word_idx, label_idx, path):
+    with open(path, 'r') as f:
+        data = f.readlines()
+        labels, sentences = labels_extraction(data)
+        labels_in_idx = []
+        for label in labels:
+            if label in label_idx:
+                labels_in_idx.append(label_idx[label])
+            else:
+                label_idx[label] = len(label_idx)
+                labels_in_idx.append(label_idx[label])
+        sentences_in_idx = []
+        for sentence in sentences:
+            sentence_idx = []
+            for word in sentence:
+                if word in word_idx:
+                    sentence_idx.append(word_idx[word])
+            sentences_in_idx.append(sentence_idx)
+        return sentences_in_idx, labels_in_idx
 
 
 def labels_extraction(data):
@@ -12,11 +51,21 @@ def labels_extraction(data):
     return labels, sentences
 
 
+def get_label_idx(labels):
+    label_list = list(set(labels))
+    label_list.append('unknown_')
+    return {label_list[i]: i for i in range(len(label_list))}
+
+
+def replace_labels_with_idx(labels, label_idx):
+    return [label_idx[labels[i]] for i in range(len(labels))]
+
+
 def remove_punctuations(sentences):
     for sentence in sentences:
         i = 0
         while i < len(sentence):
-            if sentence[i] in "?''``,.&":
+            if sentence[i] in "?''``,.&...":
                 sentence.remove(sentence[i])
             else:
                 i += 1
@@ -31,7 +80,7 @@ def to_lower_case(sentences):
 
 def remove_stop_words(sentences):
     stop_words = []
-    with open('stop_words.txt', 'r') as f:
+    with open('./src/stop_words.txt', 'r') as f:
         while True:
             line = f.readline()
             if not line:
@@ -46,6 +95,7 @@ def remove_stop_words(sentences):
                 i += 1
     return sentences
 
+
 def make_vocabulary(sentences):
     all_words = []
     for sentence in sentences:
@@ -56,22 +106,41 @@ def make_vocabulary(sentences):
     return vocabulary
 
 
-def get_preprocessed_sentences(file_path):
-    with open(file_path, 'r',encoding="utf-8") as f:
+def get_preprocessed_sentences():
+    with open('./data/train.txt', 'r') as f:
         data = f.readlines()
         _, sentences = labels_extraction(data)
         sentences = remove_punctuations(sentences)
+        sentences = remove_stop_words(sentences)
         return to_lower_case(sentences)
 
+
+# print(get_preprocessed_sentences())
+
+
 if __name__ == '__main__':
-    with open('train_1000.txt', 'r') as f:
-        data = f.readlines()
-        labels, sentences = labels_extraction(data)
-        sentences = remove_punctuations(sentences)
-        sentences = to_lower_case(sentences)
-        # sentences = remove_stop_words(sentences)
-        vocabulary = make_vocabulary(sentences)
-        with open('vocabulary.txt', 'w') as fv:
-            for word in vocabulary[:-1]:
-                fv.write(word + '\n')
-            fv.write(vocabulary[-1])
+    # with open('./train.txt', 'r') as f:
+    #     data = f.readlines()
+    #     labels, sentences = labels_extraction(data)
+    #     sentences = remove_punctuations(sentences)
+    #     sentences = to_lower_case(sentences)
+    #     # sentences = remove_stop_words(sentences)
+    #     vocabulary = make_vocabulary(sentences)
+    #     with open('vocabulary.txt', 'w') as fv:
+    #         for word in vocabulary[:-1]:
+    #             fv.write(word + '\n')
+    #         fv.write(vocabulary[-1])
+    #sentences_in_idx, labels, word_idx = process_train_set('../data/train.txt')
+    # train_int_word, train_int_label, word2idx, lable2idx = process_train_set('../data/train.txt')
+
+    '''
+    print(len(sentences_in_idx))
+    print(len(labels))
+    print(len(word_idx))
+    
+    s = 'How many points make up a perfect fivepin bowling score ?'
+    sentences_in_idx = process_single_sentence(s,word2idx)
+    print(list(lable2idx.keys())[list(lable2idx.values()).index(2)])
+    #print(sentences_in_idx)
+    print(type(lable2idx))
+    '''
--- to_be_merged/word2vec.py
+++ to_be_merged/word2vec.py
@@ -1,4 +1,6 @@
-from to_be_merged import preprocessing
+# By Maxine
+
+import preprocessing
 from collections import Counter
 import random
 
@@ -6,14 +8,13 @@ import numpy as np
 import torch
 from torch import nn
 import torch.optim as optim
-import os
-from src.question_classifier import conf
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 def create_dict(sorted_words):
     word_idx = {sorted_words[i]: i for i in range(len(sorted_words))}
     idx_word = {i: sorted_words[i] for i in range(len(sorted_words))}
+    # word_idx['unknown_'] = len(sorted_words)
+    # idx_word[len(sorted_words)] = 'unknown_'
     return word_idx, idx_word
 
 
@@ -36,30 +37,31 @@ class SkipGram(nn.Module):
 
         self.in_embedding_layer.weight.data.uniform_(-1, 1)
         self.out_embedding_layer.weight.data.uniform_(-1, 1)
-    
+
     def input_forward(self, input_words):
         return self.in_embedding_layer(input_words)
 
     def output_forward(self, output_words):
         return self.out_embedding_layer(output_words)
-    
+
     def noise_forward(self, batch_size, n_samples):
         if self.noise_dist is None:
             noise_dist = torch.ones(self.n_vocab)
         else:
             noise_dist = self.noise_dist
-            
+
         # Sample words from our noise distribution
-        noise_words = torch.multinomial(noise_dist, batch_size*n_samples, replacement=True).to('cpu')
-        #print(noise_words.shape)
-        noise_vectors = self.out_embedding_layer(noise_words).view(batch_size, n_samples, self.dim)
+        noise_words = torch.multinomial(
+            noise_dist, batch_size*n_samples, replacement=True).to('cpu')
+        noise_vectors = self.out_embedding_layer(
+            noise_words).view(batch_size, n_samples, self.dim)
         return noise_vectors
 
 
 class NegativeSampling(nn.Module):
     def __init__(self):
         super().__init__()
-    
+
     def forward(self, input_vectors, output_vectors, noise_vectors):
         batch_size, dim = input_vectors.shape
         input_vectors = input_vectors.view(batch_size, dim, 1)
@@ -69,9 +71,10 @@ class NegativeSampling(nn.Module):
         # correct log-sigmoid loss
         out_loss = torch.bmm(output_vectors, input_vectors).sigmoid().log()
         out_loss = out_loss.squeeze()
-        
+
         # incorrect log-sigmoid loss
-        noise_loss = torch.bmm(noise_vectors.neg(), input_vectors).sigmoid().log()
+        noise_loss = torch.bmm(noise_vectors.neg(),
+                               input_vectors).sigmoid().log()
         # sum the losses over the sample of noise vectors
         noise_loss = noise_loss.squeeze().sum(1)
 
@@ -86,14 +89,17 @@ def get_total_word_frequency(sentences):
         all_words.extend(sentence)
     total_count = len(all_words)
     word_count = Counter(all_words)
-    word_frequency = {word: count/total_count for word, count in word_count.items()}
+    word_frequency = {word: count/total_count for word,
+                      count in word_count.items()}
+    # word_frequency['unknown_'] = 0
     return word_frequency
 
 
 def get_target_words(context, pos, max_window_size=5):
     window_size = np.random.randint(1, max_window_size)
     left = pos-window_size if pos-window_size >= 0 else 0
-    right = pos+window_size+1 if pos+window_size < len(context) else len(context)
+    right = pos+window_size+1 if pos + \
+        window_size < len(context) else len(context)
     return context[left:pos]+context[pos+1:right]
 
 
@@ -116,32 +122,34 @@ def generate_batches(sentences, batch_size, max_window_size=5):
 
 
 def cosine_similarity(embedding, valid_size=16, valid_window=100):
-    # Here we're calculating the cosine similarity between some random words and 
-    # our embedding vectors. With the similarities, we can look at what words are 
+    # Here we're calculating the cosine similarity between some random words and
+    # our embedding vectors. With the similarities, we can look at what words are
     # close to our random words.
-    
+
     # sim = (a . b) / |a||b|
-    
+
     embed_vectors = embedding.weight
-    
+
     # magnitude of embedding vectors, |b|
     magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
-    
-    # pick N words from our ranges (0,window) and (1000,1000+window). lower id implies more frequent 
-    valid_examples = np.array(random.sample(range(valid_window), valid_size//2))
-    valid_examples = np.append(valid_examples, random.sample(range(1000,1000+valid_window), valid_size//2))
+
+    # pick N words from our ranges (0,window) and (1000,1000+window). lower id implies more frequent
+    valid_examples = np.array(random.sample(
+        range(valid_window), valid_size//2))
+    valid_examples = np.append(valid_examples, random.sample(
+        range(1000, 1000+valid_window), valid_size//2))
     valid_examples = torch.LongTensor(valid_examples).to('cpu')
-    
+
     valid_vectors = embedding(valid_examples)
     similarities = torch.mm(valid_vectors, embed_vectors.t())/magnitudes
 
     return valid_examples, similarities
 
 
-def train(vocabulary_size, embedding_dim, sentences, idx_word, batch_size=20, epochs=10):
+def train(vocabulary_size, embedding_dim, sentences, idx_word, batch_size=200, epochs=10):
     word_frequency = get_total_word_frequency(sentences)
     sorted_frequencies = np.array(sorted(word_frequency.values(), reverse=True))
-    
+
     # unigram distribution: according to the frequency that each word shows up in our corpus
     unigram_dist = sorted_frequencies/sorted_frequencies.sum()
     noise_dist = torch.from_numpy(unigram_dist**0.75/np.sum(unigram_dist**0.75))
@@ -153,6 +161,7 @@ def train(vocabulary_size, embedding_dim, sentences, idx_word, batch_size=20, ep
     steps = 0
     for e in range(epochs):
         for input_words, target_words in generate_batches(sentences, batch_size):
+            steps += 1
             inputs, targets = torch.LongTensor(input_words).to('cpu'), torch.LongTensor(target_words).to('cpu')
 
             input_vectors = model.input_forward(inputs)
@@ -165,31 +174,20 @@ def train(vocabulary_size, embedding_dim, sentences, idx_word, batch_size=20, ep
             loss.backward()
             optimizer.step()
 
-            if steps % 1000 == 0:
-                #print("Epoch: {}/{}".format(e+1, epochs))
-                #print("Loss: ", loss.item()) # avg batch loss at this point in training
+            if steps % 100 == 0:
+                print("Epoch: {}/{}".format(e+1, epochs))
+                # avg batch loss at this point in training
+                print("Loss: ", loss.item())
                 valid_examples, valid_similarities = cosine_similarity(model.in_embedding_layer)
                 _, closest_idxes = valid_similarities.topk(5)
 
                 valid_examples, closest_idxes = valid_examples.to('cpu'), closest_idxes.to('cpu')
                 for i, valid_idx in enumerate(valid_examples):
                     closest_words = [idx_word[idx.item()] for idx in closest_idxes[i]][1:]
-                    #print(idx_word[valid_idx.item()] + " | " + ', '.join(closest_words))
-                #print("...\n")
+                    print(idx_word[valid_idx.item()] + " | " + ', '.join(closest_words))
+                print("...\n")
     return model.in_embedding_layer.weight.to('cpu').data.numpy()
 
-
-def getPreTrainVector():
-    print('Please wait, pre-train...')
-    sentences = word2vec.preprocessing.get_preprocessed_sentences('../to_be_merged/train_1000.txt')
-    sorted_words = word2vec.preprocessing.make_vocabulary(sentences)
-    word_idx, idx_word = word2vec.create_dict(sorted_words)
-
-    sentences_in_idx = word2vec.replace_words_with_idx(sentences, word_idx)
-    # this
-    word_to_vec = word2vec.train(len(sorted_words), int(conf.get('param', 'word_embedding_dim')), sentences_in_idx,
-                                 idx_word)
-    return word_to_vec, word_idx
 
 if __name__ == '__main__':
     # with open('vocabulary.txt', 'r') as fv:
@@ -201,12 +199,8 @@ if __name__ == '__main__':
     #         vocabulary.append(line.strip())
     sentences = preprocessing.get_preprocessed_sentences()
     sorted_words = preprocessing.make_vocabulary(sentences)
-    print(sorted_words)
     word_idx, idx_word = create_dict(sorted_words)
-    print(word_idx)
-    print(idx_word)
     sentences_in_idx = replace_words_with_idx(sentences, word_idx)
-    word2vec = train(len(sorted_words), int(conf.get('param','word_embedding_dim')), sentences_in_idx, idx_word)
-    print('----------------')
-    print(word2vec.shape)
-    print('----------------')
+    print(len(sorted_words))
+
+    word2vec = train(len(sorted_words), 200, sentences_in_idx, idx_word)
