39ca22b92f4ee99caff4a9446685cae6d855e42b
BoxinHuang <45488458+Pakyen@users.noreply.github.com>
2021-03-12 17:19:45 +0000

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,27 +4,9 @@ from collections import Counter
 from src import word2vec
 from src import global_value as gv
 
-# 1 remove all the '?' of each sentence
-# 2 split every line into 2 lists : label and sentences
-# 3 lowercase the first letter of every sentence
-# 4 将sentences进行tokenization并去除没有用的符号与词（初步停用词）
-
-'''
-整个预处理文件的使用：
-// conf.get('param', 'path_train')表示传入的txt文件
-1. labels, sentences = sentenceProcessing(conf.get('param', 'path_train'))
-2. sentences = lower_first_letter(sentences,conf.get('param','lowercase'))
-'''
 
 from src.global_value import conf
 
-'''
-sentenceProcessing方法
-输入：txt文件
-输出：两个list，labels, sentences
-作用：将文件的每一行划分成独立的标签和句子
-使用格式：labels, sentences = sentenceProcessing(conf.get('param', 'path_train'))
-'''
 
 
 def sentence_processing(file):
@@ -42,13 +24,6 @@ def sentence_processing(file):
     return labels, sentences
 
 
-'''
-lower_first_letter方法：
-输入：list，包含了多个句子
-输出：小写化句子首字母的list
-作用：小写化句子首字母
-'''
-
 
 def lower_first_letter(sentences, isLower=True):
     if isLower:
@@ -58,10 +33,6 @@ def lower_first_letter(sentences, isLower=True):
             sentences[i] = ''.join(tmp)
     return sentences
 
-
-'''
-预处理：
-'''
 
 if __name__ == '__main__':
     labels, sentences = sentence_processing(conf.get('param', 'path_train'))
